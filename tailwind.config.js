2a41b05951cee62e59ae1f3890e569c778bcc366
AndrHar1 <andrihari0704@gmail.com>
2022-04-21 10:17:34 +0700

    sm-toys profile page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container : {
+      center : true,
+      padding : '16px',
+    },
+    extend: {
+      colors : {
+        primary : '#0f766e',
+        secondary : '#7dd3fc',
+        dark : '#0f172a',
+      },
+      screens : {
+        '2xl' : '1320px',
+      },
+    },
+  },
+  plugins: [],
+}
